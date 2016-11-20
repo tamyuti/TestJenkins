5317c1b8ab2691e9af7369d9622d191991290be0
Kabi Bhattarai <kabi.bhattarai@outlook.com>
2016-11-20 18:23:41 -0500

    Jenkins Test

---
+++ JenkinsTest/src/main/java/JenkinsTest/JenkinsTest/Sum.java
@@ -0,0 +1,9 @@
+package JenkinsTest.JenkinsTest;
+
+public class Sum {
+	
+	public int add(int x,int y){
+		return x+y;
+	}
+
+}
---
+++ JenkinsTest/src/test/java/JenkinsTest/JenkinsTest/SumTest.java
@@ -0,0 +1,28 @@
+package JenkinsTest.JenkinsTest;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SumTest {
+	Sum s;
+	
+	@BeforeClass
+	public void createObj(){
+		s=new Sum();
+	}
+	
+	@Test
+	public void posAdd(){
+		int num=s.add(5, 10);
+		Assert.assertEquals(num, 15);
+	}
+	
+	@Test
+	public void negAdd(){
+		int num=s.add(-5, -10);
+		Assert.assertEquals(num, -15);
+	}
+	
+
+}
